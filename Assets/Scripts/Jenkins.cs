dc529e6a3c84e7329e8eb59aff5ee1977f6e9b02
김영진 <ff-ff@naver.com>
2024-11-25 23:35:11 +0900

    Update Jenkins.cs

--- Assets/Scripts/Jenkins.cs
+++ Assets/Scripts/Jenkins.cs
@@ -7,7 +7,7 @@ public class Jenkins
     //dd
 #if UNITY_EDITOR
     //[MenuItem("Build/Standalone Windows")]
-    public static void PerformBuild()
+    public static void PerformPCBuild()
     {
         BuildPlayerOptions options = new BuildPlayerOptions();
         // 씬 추가
@@ -22,6 +22,26 @@ public class Jenkins
         options.locationPathName = "C:/Builds/UnityWindowsBuild/Build.exe";
         // 빌드 타겟
         options.target = BuildTarget.StandaloneWindows64;
+
+        // 빌드
+        BuildPipeline.BuildPlayer(options);
+    }
+
+    public static void PerformWebGLBuild()
+    {
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        // 씬 추가
+        List<string> scenes = new List<string>();
+        foreach (var scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            scenes.Add(scene.path);
+        }
+        options.scenes = scenes.ToArray();
+        // 타겟 경로(빌드 결과물이 여기 생성됨)
+        options.locationPathName = "C:/WebGLBuild/Build";
+        // 빌드 타겟
+        options.target = BuildTarget.WebGL;
 
         // 빌드
         BuildPipeline.BuildPlayer(options);
